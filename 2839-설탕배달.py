a696a02d64fe2ceba345caa900de45a6abf48241
kimkyuhong <bobo3766@naver.com>
2024-09-07 19:05:30 +0900

    9/7 문제풀이

--- 2839-설탕배달.py
+++ 2839-설탕배달.py
@@ -3,6 +3,16 @@ input = sys.stdin.readline
 
 N = int(input().strip())
 
-graph = [[-1]*1000]*1700
+graph = [-1]*5001
+graph[3] = 1
+graph[5] = 1
 
-print(graph)
+for i in range(6, 5001):
+    if graph[i-3] > 0 and graph[i-5] > 0:
+        graph[i] = min(graph[i-3], graph[i-5])+1
+    elif graph[i-5] > 0 :
+        graph[i] = graph[i-5]+1
+    elif graph[i-3] > 0 :
+        graph[i] = graph[i-3]+1
+
+print(graph[N])
