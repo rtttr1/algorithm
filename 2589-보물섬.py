693acdd26cd936edd87b60abeb1433906f54dc95
kimkyuhong <bobo3766@naver.com>
2024-11-08 08:01:39 +0900

    11/8 문제풀이

--- 2589-보물섬.py
+++ 2589-보물섬.py
@@ -4,37 +4,37 @@ input = sys.stdin.readline
 
 N, M = map(int, sys.stdin.readline().strip().split(' '))
 
-graph = []
+graph = [list(map(str, input().rstrip())) for _ in range(N)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-answer = 100000000000
-
-for i in range(N) :
-    graph.append(list(map(str, input().rstrip())))
+answer = 0
 
 def BFS(a,b,visited):
     queue = deque([[a,b,0]])
+    visited[b][a] = True
     max = 0
+
     while queue:
-        x, y, count = queue.pop()
+        x, y, count = queue.popleft()
         max = count
 
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < M and 0 <= ny < N and graph[ny][nx] == 'L' and not visited[ny][nx] :
+
+            if nx < 0 or nx >= M or ny < 0 or ny >= N or visited[ny][nx]:
+                continue 
+            
+            if graph[ny][nx] == 'L' :
                 queue.append([nx, ny, count+1])
                 visited[ny][nx] = True
+
     return max
 
 for i in range(N):
     for j in range(M):
         visited = [[False]*M for _ in range(N)]
         if graph[i][j] == 'L':
-            temp = BFS(j, i, visited) 
-            print(temp)
-            if temp < answer:
-                answer = temp
-            
-        
+            answer = max(answer, BFS(j, i, visited))
+                   
 print(answer)
