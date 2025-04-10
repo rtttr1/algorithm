3e2849f7bc739b45c636ac036fc1af447d567b18
kimkyuhong <94050324+rtttr1@users.noreply.github.com>
2025-04-10 13:53:57 +0900

    [Silver V] Title: 문서 검색, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -5,13 +5,5 @@ arr = str(input().rstrip())
 word = str(input().rstrip())
 N = len(word)
 
-def search(temp):    
-    temp = temp.replace(word, '!')
-    return temp.count('!')
-
-answer = 0
 temp = arr.replace(word, '!')
-# for i in range(len(arr)-N):
-#     copy = arr[i:]
-#     answer = max(answer, search(copy))
 print(temp.count('!'))
