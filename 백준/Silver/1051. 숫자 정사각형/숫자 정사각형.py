09e6bf5470ad6bfc3fbd16e91b78e46428a9b2e3
kimkyuhong <94050324+rtttr1@users.noreply.github.com>
2025-04-13 21:17:27 +0900

    [Silver III] Title: 숫자 정사각형, Time: 108 ms, Memory: 110988 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [list(map(int, input().strip())) for _ in range(N)]
+
+answer = 1
+def search(x, y):
+    global answer
+    num = graph[x][y]
+    
+    for i in range(x+1, N):
+        if graph[i][y] == num:
+            cm = i - x
+            if cm + y < M and graph[x][y+cm] == num and graph[i][y+cm] == num:
+                answer = max(answer, (cm+1)**2)
+
+
+
+for x in range(N):
+    for y in range(M):
+        search(x,y)
+
+print(answer)
