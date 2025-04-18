c09948964e957f319200e5e58720a1717f0fcd97
kimkyuhong <94050324+rtttr1@users.noreply.github.com>
2025-04-18 11:10:50 +0900

    [Silver IV] Title: 기타줄, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -0,0 +1,19 @@
+import sys
+import math
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+minSet, minOne = 10e9, 10e9
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    minSet, minOne = min(minSet, a), min(minOne, b)
+
+
+if minOne*6 <= minSet:
+    print(minOne*N)
+else:
+    answer = math.ceil(N / 6) * minSet
+    answer = min(answer, math.floor(N / 6) * minSet +  minOne*(N - math.floor(N / 6)*6))
+    print(answer)
+
