bea9e251926b06d8158177b624eb45185eafb8bd
kimkyuhong <94050324+rtttr1@users.noreply.github.com>
2025-04-21 20:52:44 +0900

    [Silver II] Title: K번째 소수, Time: 116 ms, Memory: 117360 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/15965. K번째 소수/K번째 소수.py
@@ -0,0 +1,24 @@
+import sys
+import math
+input = sys.stdin.readline
+
+K = int(input().rstrip())
+
+MAX = 1000000
+prime = [True for i in range(MAX+1)]
+prime[1] = False
+
+for i in range(2, int(math.sqrt(MAX))+1):
+    if prime[i] == True:
+        j = 2
+        while i*j <= MAX:
+            prime[i*j] = False
+            j+=1
+
+count = 0
+for i in range(2, MAX+1):
+    if prime[i]:
+        count += 1
+    if count == K:
+        print(i)
+        break
